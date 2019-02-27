8dd96e90d8e57705dba680c78b6c1ed23bba6455
Ricardo N Feliciano <FelicianoTech@gmail.com>
2019-02-27 10:01:01 -0500

    Support passing in a port via CLI. (#4)

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"os"
+	"strconv"
+
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+
+	var port string
+
+	if len(os.Args) == 2 {
+		port = os.Args[1]
+	} else {
+		port = "1107"
+	}
+
+	if portInt, err := strconv.ParseInt(port, 10, 32); err != nil {
+		log.Fatal("Invalid port number provided: " + port)
+	} else {
+
+		if portInt < -1 || portInt > 65535 {
+			log.Fatal("Invalid port number provided: " + port)
+		}
+	}
 
 	log.Info("Starting server...")
 
@@ -13,6 +33,5 @@ func main() {
 
 	log.Info("Server initialized.")
 
-	server.run(":1107")
-	//server.run(":80")
+	server.run(":" + port)
 }
